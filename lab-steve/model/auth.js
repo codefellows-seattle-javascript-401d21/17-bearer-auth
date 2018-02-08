811d7b45b8c3a560d1963d9648fc594b0d8f56b9
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-07 20:21:56 -0800

    Adding spacing and semicolon

--- lab-steve/model/auth.js
+++ lab-steve/model/auth.js
@@ -14,19 +14,19 @@ const Auth = mongoose.Schema({
 
 
 Auth.methods.generatePasswordHash = function(password) {
-  if(!password) return Promise.reject(new Error('Authorization failed. Password required.'));
+  if (!password) return Promise.reject(new Error('Authorization failed. Password required.'));
 
   return bcrypt.hash(password, 10)
     .then(hash => this.password = hash)
     .then(() => this)
     .catch(err => err);
-}
+};
 
 Auth.methods.comparePasswordHash = function(password) {
   return new Promise((resolve, reject) => {
     bcrypt.compare(password, this.password, (err, valid) => {
-      if(err) return reject(err);
-      if(!valid) return reject(new Error('Authorization failed. Password invalid.'));
+      if (err) return reject(err);
+      if (!valid) return reject(new Error('Authorization failed. Password invalid.'));
       resolve(this);
     });
   });
@@ -37,12 +37,12 @@ Auth.methods.generateCompareHash = function() {
   return this.save()
     .then(() => Promise.resolve(this.compareHash))
     .catch(() => this.generateCompareHash());
-}
+};
 
 Auth.methods.generateToken = function() {
   return this.generateCompareHash()
     .then(compareHash => jwt.sign({token: compareHash}, process.env.APP_SECRET))
     .catch(err => err);
-}
+};
 
 module.exports = mongoose.model('auth', Auth);
