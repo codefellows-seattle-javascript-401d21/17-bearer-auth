91623bf4f295ca510221b725df3502bda1b65a9c
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-07 21:49:12 -0800

    Replaced redundant strings with const var

--- lab-steve/__test__/routes/car-post.test.js
+++ lab-steve/__test__/routes/car-post.test.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const faker = require('faker');
-const mocks = require('../lib/mocks');
 const superagent = require('superagent');
+
+const mocks = require('../lib/mocks');
 const server = require('../../lib/server');
 require('jest');
+
+const PORT = process.env.PORT;
+const CAR_ENDPOINT = `:${PORT}/api/v1/car`;
 
 describe('POST /api/v1/car', () => {
   beforeAll(server.start);
@@ -21,7 +25,7 @@ describe('POST /api/v1/car', () => {
       return mocks.car.createOne()
         .then(mock => carMock = mock)
         .then(mock => {
-          return superagent.post(`:${process.env.PORT}/api/v1/car`)
+          return superagent.post(`${CAR_ENDPOINT}`)
             .set('Authorization', `Bearer ${mock.token}`)
             .send({
               make: faker.random.words(1),
@@ -40,13 +44,13 @@ describe('POST /api/v1/car', () => {
 
   describe('Invalid', () => {
     it('should return a 401 status (NOT AUTHORIZED) with a bad token', () => {
-      return superagent.post(`:${process.env.PORT}/api/v1/car`)
+      return superagent.post(`${CAR_ENDPOINT}`)
         .set('Authorization', 'Bearer BADTOKEN')
         .catch(err => expect(err.status).toEqual(401));
     });
 
     it('should return a 400 status (BAD REQUEST) with an improperly formatted body', () => {
-      return superagent.post(`:${process.env.PORT}/api/v1/car`)
+      return superagent.post(`${CAR_ENDPOINT}`)
         .set('Authorization', `Bearer ${this.mockUser.token}`)
         .send({})
         .catch(err => expect(err.status).toEqual(400));
